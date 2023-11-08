909e3023f399bbe6883f535a037d44cfbcc1e82b
Monte-Ntuli <86322309+Monte-Ntuli@users.noreply.github.com>
2023-11-08 19:58:44 +0200

    Create function

--- Console App 1/ConsoleApp1/Program.cs
+++ Console App 1/ConsoleApp1/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+public static int SumOfEvenNumbers(int[] numbers)
+{
+    int sum = 0;
+    foreach (var number in numbers)
+    {
+        if (number % 2 == 0)
+        {
+            sum += number;
+        }
+    }
+    return sum;
+}
