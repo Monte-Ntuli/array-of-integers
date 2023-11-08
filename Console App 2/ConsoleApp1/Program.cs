c2025817d7e64f60ff72f8b447847847cea41c13
Monte-Ntuli <86322309+Monte-Ntuli@users.noreply.github.com>
2023-11-08 20:02:29 +0200

    console app 2

---
+++ Console App 2/ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public static int SumOfEvenNumbers(int[] numbers)
+{
+    return numbers.Where(n => n % 2 == 0).Sum();
+}
